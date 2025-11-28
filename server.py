43f613de97379f9be645fb59aac446362e52e211
satyendrapawar214 <satyendrapawar214@gmail.com>
2025-11-28 12:17:14 +0530

    commit 281125

--- server.py
+++ server.py
@@ -1,68 +1,22 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import json, os
 
-STATIC_DIR = "static"
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Send response status code
+        self.send_response(200)
 
-class MyHandler(BaseHTTPRequestHandler):
-
-    def _set_headers(self, status=200, content_type="text/html"):
-        self.send_response(status)
-        self.send_header("Content-Type", content_type)
+        # Send headers
+        self.send_header('Content-type', 'text/plain')
         self.end_headers()
 
-    def serve_static_file(self, filename="index.html"):
-        filepath = os.path.join(STATIC_DIR, filename)
+        # Send message body
+        self.wfile.write(b"OK")
 
-        if not os.path.exists(filepath):
-            self._set_headers(404, "text/plain")
-            self.wfile.write(b"404 File Not Found")
-            return
+def run(server_class=HTTPServer, handler_class=SimpleHandler, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting server on port {port}...')
+    httpd.serve_forever()
 
-        ext = filename.split(".")[-1]
-        content_type = {
-            "html": "text/html",
-            "js": "application/javascript",
-            "css": "text/css"
-        }.get(ext, "text/plain")
-
-        self._set_headers(200, content_type)
-        with open(filepath, "rb") as f:
-            self.wfile.write(f.read())
-
-    def do_GET(self):
-        if self.path == "/" or self.path == "/index.html":
-            self.serve_static_file("index.html")
-
-        elif self.path == "/json":
-            self._set_headers(200, "application/json")
-            response = {"message": "Hello from merged server!"}
-            self.wfile.write(json.dumps(response).encode())
-
-        else:
-            # Attempt to serve other static files
-            requested = self.path.strip("/")
-            self.serve_static_file(requested)
-
-    def do_POST(self):
-        content_length = int(self.headers.get("Content-Length", 0))
-        body = self.rfile.read(content_length)
-
-        try:
-            data = json.loads(body)
-        except:
-            data = {"error": "Invalid JSON"}
-
-        self._set_headers(200, "application/json")
-        response = {"received": data}
-        self.wfile.write(json.dumps(response).encode())
-
-
-def run_server():
-    host = "0.0.0.0"
-    port = int(os.environ.get("PORT", 8000))
-    print(f"Server running on port {port}")
-    server = HTTPServer((host, port), MyHandler)
-    server.serve_forever()
-
-if __name__ == "__main__":
-    run_server()
+if __name__ == '__main__':
+    run()
